bf4a2778acea4f82e33a818aed2d276f5bfceae3
jiayu-chen706 <jiayuchen706@gmail.com>
2025-06-06 20:04:20 +1000

    Add environment

--- src/client/src/api/urls.js
+++ src/client/src/api/urls.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://ha-redback.onrender.com";
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://127.0.0.1:5001";
+
 // export const BASE_URL = "http://127.0.0.1:5001";
